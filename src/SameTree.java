29230aaa2a7a709c3c7b13d1cfc81657e4f3f76c
wangbeishan <wangbeishan1900@gmail.com>
2023-01-06 23:12:07 +0800

    06/1/2023

---
+++ src/SameTree.java
@@ -0,0 +1,28 @@
+import java.util.Stack;
+
+public class SameTree {
+    class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    public boolean isSameTree(TreeNode p, TreeNode q) {
+        if (p == null && q == null) return true;
+        if (p == null || q == null || p.val != q.val) return false;
+        return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+    }
+}
